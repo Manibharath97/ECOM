2f414f88bb19556238cdfbcfab9fa330cea83cf1
Manibharath97 <manibharath.ch@gmail.com>
2025-08-31 17:21:25 +0530

    exposed actuators and added @Component in service

---
+++ src/main/java/com/codesnippet/ecom/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.codesnippet.ecom.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductService {
+
+	public ProductService() {
+		System.out.println("ProductService");
+	}
+}
