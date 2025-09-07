06034f6c86d9eb4acb99a70d85d5907e43ea060b
Manibharath97 <manibharath.ch@gmail.com>
2025-09-07 20:10:11 +0530

    Used excludeFilters to exclude mentioned classes from runtime

--- src/main/java/com/codesnippet/ecom/config/EcomConfig.java
+++ src/main/java/com/codesnippet/ecom/config/EcomConfig.java
@@ -2,9 +2,17 @@ package com.codesnippet.ecom.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import com.codesnippet.ecomassistant.service.DepricatedUtilityService;
 
 @Configuration
-@ComponentScan(basePackages = "com.codesnippet.ecomassistant")
+@ComponentScan(
+		basePackages = "com.codesnippet.ecomassistant",
+		excludeFilters = @ComponentScan.Filter
+		(type = FilterType.ASSIGNABLE_TYPE,
+		classes = DepricatedUtilityService.class)
+)
 public class EcomConfig {
 
 }
---
+++ src/main/java/com/codesnippet/ecomassistant/service/DepricatedUtilityService.java
@@ -0,0 +1,11 @@
+package com.codesnippet.ecomassistant.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepricatedUtilityService {
+
+	public DepricatedUtilityService() {
+		System.out.println("DepricatedUtilityService");
+	}
+}
